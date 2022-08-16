599fb89cf22cb82d07a463ee6dfc9ec6892d7208
Your Name <you@example.com>
2022-08-16 12:29:10 -0300

    att v1

--- AppMinhasCompras/AppMinhasCompras/App.xaml.cs
+++ AppMinhasCompras/AppMinhasCompras/App.xaml.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.IO;
+using AppMinhasCompras.Helper;
 
 namespace AppMinhasCompras
 {
     public partial class App : Application
     {
+
+        static SQLiteDatabaseHelper database;
+
+        public static SQLiteDatabaseHelper Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "arquivo.db3"
+                    );
+
+
+                    database = new SQLiteDatabaseHelper(path);
+                }
+
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new View.Lista());
         }
 
         protected override void OnStart()
