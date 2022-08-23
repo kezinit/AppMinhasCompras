c61ac344e8453f4a71ca0a8a781865aa0cabf470
Usuario <Usuario@CPD02-M03>
2022-08-23 12:29:51 -0300

    deixando bunito

--- AppMinhasCompras/AppMinhasCompras.Android/MainActivity.cs
+++ AppMinhasCompras/AppMinhasCompras.Android/MainActivity.cs
@@ -8,6 +8,7 @@ using Android.OS;
 namespace AppMinhasCompras.Droid
 {
     [Activity(Label = "AppMinhasCompras", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
